b97e7adadf2c61b807d0738575fb0acd6f88f1ce
Atul Dhuvad <atuldhuvad@gmail.com>
2026-08-04 14:34:25 +0530

    Integrate India Inspiration Studio app logo

--- App/sw.js
+++ App/sw.js
@@ -1,5 +1,5 @@
-const CACHE="daily-inspiration-v2-3-final";
-const FILES=["./","./index.html","./manifest.webmanifest","./assets/icon-192.svg","./assets/icon-512.svg"];
+const CACHE="daily-inspiration-v2-3-final-logo";
+const FILES=["./","./index.html","./manifest.webmanifest","./assets/icon-192.png","./assets/icon-512.png","./assets/apple-touch-icon.png","./assets/favicon.ico"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
