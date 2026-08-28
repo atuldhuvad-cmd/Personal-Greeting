5f8aa2ac4787f0928d6d6a5f8d6127396cd3c97e
atuldhuvad-cmd <atuldhuvad@gmail.com>
2026-08-28 08:45:52 -0400

    Add festival poster templates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wishcraft-v14';
-const APP=['./','./index.html','./css/styles.css','./css/fixes.css','./js/messages.js','./js/templates.js','./js/app.js','./manifest.json','./assets/icon.svg'];
+const CACHE='wishcraft-v15';
+const APP=['./','./index.html','./css/styles.css','./css/fixes.css','./js/messages.js','./js/templates.js','./js/app.js','./manifest.json','./assets/icon.svg','./assets/festival/diwali.png','./assets/festival/holi.png','./assets/festival/janmashtami.png','./assets/festival/navratri.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{if(response.ok&&new URL(event.request.url).origin===location.origin){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
